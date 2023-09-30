fc8a10d9a455dd1fff86d219de483349d6f25977
qscxzsewqazxcde@gmail.com <qscxzsewqazxcde@gmail.com>
2023-09-30 18:08:08 +0300

    refactor: small changes in root command

--- cmd/root.go
+++ cmd/root.go
@@ -38,21 +38,16 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "clisheer",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A command line companion for casheer expense manager.",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
+// Execute adds all child commands to the root command and sets flagsmain() appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
